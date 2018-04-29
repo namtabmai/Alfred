a18e9070379957be28f1592780c87885864d4766
Andrew West <andrew@keyboardcowboy.co.uk>
2018-04-29 13:54:20 +0100

    - Standarised quotation mark usage - Added default image for posts without an image

--- reddit.py
+++ reddit.py
@@ -3,31 +3,32 @@ import discord
 import feedparser
 import logging
 import html2text
+import random
 import re
 
 class Reddit:
     def __init__(self, config):
-        logging.info("Reddit::__init__")
+        logging.info('Reddit::__init__')
 
         self.config = config
 
         self.update_frequency = 300
         try:
-            self.update_frequency = int(self.config.get("reddit_update_frequency", fallback=300))
+            self.update_frequency = int(self.config.get('reddit_update_frequency', fallback=300))
         except:
-            logging.exception("Could not initialise reddit")
+            logging.exception('Could not initialise reddit')
 
-        logging.info("Update frequency {0}".format(self.update_frequency))
+        logging.info('Update frequency {0}'.format(self.update_frequency))
 
     def get_rss_posts(self, subreddit):
-        logging.debug("Reddit::get_rss_posts")
+        logging.debug('Reddit::get_rss_posts')
 
         new_posts = []
         try:
             rss = feedparser.parse('https://rss.reddit.com/r/{0}/new'.format(subreddit))
 
             last_updated = [reddit.last_updated for reddit in self.config.get_reddit() if reddit.subreddit == subreddit][0]
-            logging.debug("Checking for updated to {0} since {1}".format(subreddit, last_updated))
+            logging.debug('Checking for updated to {0} since {1}'.format(subreddit, last_updated))
 
             # Just work out the last update time, presume all the posts have already been seen
             for entry in rss['entries'][::-1]:
@@ -53,16 +54,23 @@ class Reddit:
         matches = re.search(r'(src|href)="([^"]+?\.(png|gif|jpg|jpeg))"', content)
         if matches is not None:
             logging.debug('Found image: {0}'.format(matches.group(2)))
-            embed.set_thumbnail(url=matches.group(2))
+            url = matches.group(2)
+        else:
+            url = 'https://www.redditstatic.com/icon.png'
+
+        embed.set_thumbnail(url=url)
 
     def add_user(self, post, embed):
-        author = post.get("author", "deleted")
-        author = author if author == "deleted" else post.author_detail.name[3:]
+        author = post.get('author', 'deleted')
+        author = author if author == 'deleted' else post.author_detail.name[3:]
 
-        shit_post = True if author.lower() in self.config.get("shit_posters", "").split(",") else False
+        shit_post = True if author.lower() in self.config.get('shit_posters', '').split(',') else False
 
-        name = "New shit post by" if shit_post else "New post by"
-        link = "" if author == "deleted" else "[{0}]({1})".format(author, post.author_detail.href)
+        name = 'New shit post by' if shit_post else 'New post by'
+        link = '' if author == 'deleted' else '[{0}]({1})'.format(author, post.author_detail.href)
+
+        if shit_post:
+            embed.add_field(name='Shit post level', value=':poop:'*random.randint(1,5))
 
         embed.add_field(name=name, value=link, inline=False)
 
@@ -72,18 +80,20 @@ class Reddit:
         h.ignore_images = True
         h.body_width = 0
 
-        synopsisWordCount = int(self.config.get("reddit_synopsis_word_count", 26))
+        synopsisWordCount = int(self.config.get('reddit_synopsis_word_count', 26))
 
         description = h.handle(post.summary)
         # Strip out the comments link
-        description = re.sub(r"\[\[comments\]\]\([^)]+\)", "", description)
+        description = re.sub(r'\[\[comments\]\]\([^)]+\)', '', description)
         # Strip out the link
-        description = re.sub(r"\[\[link\]\]\([^)]+\)", "", description)
+        description = re.sub(r'\[\[link\]\]\([^)]+\)', '', description)
         # Strip out the submitter link
-        description = re.sub(r"submitted by (\[[^\]]+]\([^\)]+\))", "", description)
+        description = re.sub(r'submitted by (\[[^\]]+]\([^\)]+\))', '', description)
 
         # Take the first 27 words from the summary
-        description = " ".join(description.split()[0:synopsisWordCount]) + "..."
+        description = ' '.join(description.split()[0:synopsisWordCount])
+        if len(description) is not 0:
+            description = description + '...'
 
         colour = int(subreddit.colour, 0) if subreddit.colour is not None else discord.Embed.Empty
 
@@ -99,15 +109,17 @@ class Reddit:
 
         message = self.format_post(subreddit, post)
 
-        await client.send_message(channel, embed=message)
+        temp_message = await client.send_message(channel, embed=message)
+
+        client.get_message(channel, temp_message.id)
 
     async def check_feeds(self, client):
-        logging.info("Reddit::check_feeds")
+        logging.info('Reddit::check_feeds')
 
         await client.wait_until_ready()
 
         for r in self.config.get_reddit():
-            logging.info("Announcing the subreddit {0} to server {1} ({2}), channel {3} ({4})".format(
+            logging.info('Announcing the subreddit {0} to server {1} ({2}), channel {3} ({4})'.format(
                 r.subreddit,
                 r.server['name'],
                 r.server['discord_id'],
@@ -128,7 +140,7 @@ class Reddit:
                     continue
 
                 for channel in [client.get_channel(reddit.channel['discord_id']) for reddit in reddit_data if reddit.subreddit == subreddit.subreddit]:
-                    logging.debug("Posting all new posts for subreddit {0} to {1}:{2}".format(subreddit.subreddit, channel.server.name, channel.name))
+                    logging.debug('Posting all new posts for subreddit {0} to {1}:{2}'.format(subreddit.subreddit, channel.server.name, channel.name))
                     for post in new_posts:
                         await self.send_reddit_link(client, channel, subreddit, post)
             await asyncio.sleep(self.update_frequency)
